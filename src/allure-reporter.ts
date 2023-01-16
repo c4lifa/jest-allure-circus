8a6a3ac18618e126a7405f546a167f4f77a8e5f1
KillWolfVlad <KillWolfVlad@users.noreply.github.com>
2023-01-16 09:54:58 +0500

    feat: add new suites strategy

--- src/allure-reporter.ts
+++ src/allure-reporter.ts
@@ -22,6 +22,11 @@ import type * as jest from '@jest/types';
 import JestAllureInterface, {ContentType, Labels} from './jest-allure-interface';
 import defaultCategories from './category-definitions';
 
+export enum AllureReporterSuitesStrategy {
+	legacy = 'legacy',
+	latestDescribe = 'latestDescribe',
+}
+
 export default class AllureReporter {
 	currentExecutable: ExecutableItemWrapper | null = null;
 	private readonly allureRuntime: AllureRuntime;
@@ -32,7 +37,8 @@ export default class AllureReporter {
 	private readonly tmsUrl: string;
 	private readonly categories: Category[] = defaultCategories;
 	private readonly testNames: Array<String> = [];
-	private readonly addCodeInReport: boolean = true
+	private readonly addCodeInReport: boolean = true;
+	private readonly suitesStrategy: AllureReporterSuitesStrategy;
 	labels: Labels[] = [];
 
 	constructor(options: {
@@ -42,7 +48,8 @@ export default class AllureReporter {
 		environmentInfo?: Record<string, string>;
 		categories?: Category[];
 		labels: Labels[];
-		addCodeInReport?: boolean
+		addCodeInReport?: boolean;
+		suitesStrategy: AllureReporterSuitesStrategy;
 	}) {
 		this.allureRuntime = options.allureRuntime;
 
@@ -64,6 +71,8 @@ export default class AllureReporter {
 		if ('addCodeInReport' in options) {
 			this.addCodeInReport = Boolean(options.addCodeInReport)
 		}
+
+		this.suitesStrategy = options.suitesStrategy;
 
 		this.allureRuntime.writeCategoriesDefinitions(this.categories);
 	}
@@ -381,6 +390,14 @@ export default class AllureReporter {
 	}
 
 	private addSuiteLabelsToTestCase(currentTest: AllureTest, testPath: string): AllureTest {
+		if (this.suitesStrategy === AllureReporterSuitesStrategy.latestDescribe) {
+			if (this.currentSuite) {
+				currentTest.addLabel(LabelName.SUITE, this.currentSuite.name);
+			}
+
+			return currentTest;
+		}
+
 		const isWindows = os.type() === 'Windows_NT';
 		const pathDelimiter = isWindows ? '\\' : '/';
 		const pathsArray = testPath.split(pathDelimiter);
